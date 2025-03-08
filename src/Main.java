2f1c2669e22f2c74f5566c33cd5db76097f133e2
berke sertel <berkesertel4@gmail.com>
2025-03-08 11:47:14 +0300

    ArrayUtil class has been updated

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import org.arrayutil.app.ArrayUtil;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int[] arr = ArrayUtil.getRandomArray(10);
+        ArrayUtil.printArray(arr);
+        int secondMax = ArrayUtil.secondMax(arr);
+        System.out.println("SecondMax: " + secondMax);
+        ArrayUtil.bubbleSort(arr);
+        ArrayUtil.printArray(arr);
     }
 }
